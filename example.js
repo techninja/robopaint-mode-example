1155fb4c05b4b5d4c1dce2411f0a72ed99555b4d
techninja <james@tn42.com>
2015-10-13 16:31:50 -0700

    Add full control buttons, and control "panel" Also bump version

--- example.js
+++ example.js
@@ -10,6 +10,7 @@ var actualPen = {}; // Hold onto the latest actualPen object from updates.
 var buffer = {};
 var canvas = rpRequire('canvas');
 var hershey = require('hersheytext');
+var t = i18n.t; // The mother of all shortcuts
 var textHasRendered = false; // First run boolean
 
 mode.pageInitReady = function () {
@@ -58,6 +59,22 @@ function paperLoadedInit() {
   mode.run('up');
 }
 
+// Catch CNCServer buffered callbacks
+mode.onCallbackEvent = function(name) {
+  switch (name) {
+    case 'autoPaintBegin': // Should happen when we've just started
+      $('#pause').prop('disabled', false); // Enable pause button
+      break;
+    case 'autoPaintComplete': // Should happen when we're completely done
+      $('#pause').attr('class', 'ready')
+        .text(robopaint.t('common.action.start'))
+        .prop('disabled', false);
+      $('#buttons button.normal').prop('disabled', false); // Enable options
+      $('#cancel').prop('disabled', true); // Disable the cancel print button
+      break;
+  }
+};
+
 // Bind all controls (happens before pageInitReady, @see mode.preload.js)
 mode.bindControls = function() {
   // Populate font choices
@@ -87,7 +104,7 @@ mode.bindControls = function() {
   }).val('futural'); // Set default font
 
   // Re-render on keypress/change
-  $('input').on('input change', function(e){
+  $('input, textarea').on('input change', function(e){
     $('#fontselect').change();
   });
 
@@ -98,17 +115,106 @@ mode.bindControls = function() {
     );
   });
 
-  // Bind print functionality
-  $('#print').click(function() {
-    // Auto Paint requires everything on the layer be ungrouped, so we ungroup
-    // everything first, then autoPaint.
-    paper.utils.ungroupAllGroups(paper.canvas.actionLayer);
-    paper.utils.autoPaint(paper.canvas.actionLayer);
+  // Cancel Print
+  $('#cancel').click(function(){
+    var cancelPrint = confirm(t("common.action.cancelconfirm"));
+    if (cancelPrint) {
+      mode.onCallbackEvent('autoPaintComplete');
+      mode.fullCancel(mode.t('status.cancelled'));
+    }
+  });
 
-    // The ungrouped elements aren't terribly useful, so we can just delete them
-    // then re-render them by triggering a fontselect change.
-    paper.canvas.actionLayer.removeChildren();
-    $('#fontselect').change();
+  // Bind pause click and functionality
+  $('#pause').click(function() {
+
+    // With nothing in the queue, start autopaint!
+    if (buffer.length === 0) {
+      $('#pause')
+        .removeClass('ready')
+        .attr('title', t("modes.print.status.pause"))
+        .text(t('common.action.pause'))
+        .prop('disabled', true);
+      $('#buttons button.normal').prop('disabled', true); // Disable options
+      $('#cancel').prop('disabled', false); // Enable the cancel print button
+
+      // Auto Paint requires everything on the layer be ungrouped, so we ungroup
+      // everything first, then autoPaint.
+      paper.utils.ungroupAllGroups(paper.canvas.actionLayer);
+      paper.utils.autoPaint(paper.canvas.actionLayer);
+
+      // The ungrouped elements aren't terribly useful, so we can just delete them
+      // then re-render them by triggering a fontselect change.
+      paper.canvas.actionLayer.removeChildren();
+      $('#fontselect').change();
+
+    } else {
+      // With something in the queue... we're either pausing, or resuming
+      if (!buffer.paused) {
+        // Starting Pause =========
+        $('#pause').prop('disabled', true).attr('title', t("status.wait"));
+        mode.run([
+          ['status', t("status.pausing")],
+          ['pause']
+        ], true); // Insert at the start of the buffer so it happens immediately
+
+        mode.onFullyPaused = function(){
+          mode.run('status', t("status.paused"));
+          $('#buttons button.normal').prop('disabled', false); // Enable options
+          $('#pause')
+            .addClass('active')
+            .attr('title', t("status.resume"))
+            .prop('disabled', false)
+            .text(t("common.action.resume"));
+        };
+      } else {
+        // Resuming ===============
+        $('#buttons button.normal').prop('disabled', true); // Disable options
+        mode.run([
+          ['status', t("status.resuming")],
+          ['resume']
+        ], true); // Insert at the start of the buffer so it happens immediately
+
+        mode.onFullyResumed = function(){
+          $('#pause')
+            .removeClass('active')
+            .attr('title', t("mode.print.status.pause"))
+            .text(t('common.action.pause'));
+          mode.run('status', t("status.resumed"));
+        };
+      }
+    }
+  });
+
+  // Bind to control buttons
+  $('#park').click(function(){
+    // If we're paused, skip the buffer
+    mode.run([
+      ['status', t("status.parking"), buffer.paused],
+      ['park', buffer.paused], // TODO: If paused, only one message will show :/
+      ['status', t("status.parked"), buffer.paused]
+    ]);
+  });
+
+
+  $('#pen').click(function(){
+    // Run height pos into the buffer, or skip buffer if paused
+    var newState = 'up';
+    if (actualPen.state === "up" || actualPen.state === 0) {
+      newState = 'down';
+    }
+
+    mode.run(newState, buffer.paused);
+  });
+
+  // Motor unlock: Also lifts pen and zeros out.
+  $('#disable').click(function(){
+    mode.run([
+      ['status', t("status.unlocking")],
+      ['up'],
+      ['zero'],
+      ['unlock'],
+      ['status', t("status.unlocked")]
+    ]);
   });
 }
 
@@ -133,11 +239,11 @@ mode.onPenUpdate = function(botPen){
 
   // Update button text/state
   // TODO: change implement type <brush> based on actual implement selected!
-  /*var key = 'common.action.brush.raise';
+  var key = 'common.action.brush.raise';
   if (actualPen.state === "up" || actualPen.state === 0){
     key = 'common.action.brush.lower';
   }
-  $('#pen').text(t(key));*/
+  $('#pen').text(t(key));
 }
 
 // An abbreviated buffer update event, contains paused/not paused & length.
